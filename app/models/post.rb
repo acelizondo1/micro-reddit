5da146cab9f900fcf75ce112e2656417b60a73ed
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-05 09:15:14 -0500

    Adding comment model with associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 2 }
 
     belongs_to :user
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     validates :username, presence: true, length: { maximum: 10 }, uniqueness: true
 
     has_many :posts
+    has_many :comments
 end
